27a34889d976364648cee26d6a22e6bc673545fb
faheem <faheem@faheempatel.com>
2016-11-17 22:03:48 +0000

    Initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,80 @@
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body { height: 100%; }
+
+  body {
+    align-items: center;
+    background:
+      radial-gradient(
+        ellipse at center,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(229, 229, 229, .85) 100%
+      );
+    background-position: center;
+    display: flex;
+    font-family: Lato, Helvetica, sans-serif;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
+
+<template>
+  <div>
+    <calc 
+      label="Price" 
+      v-model="price"
+      v-bind:currency="currency"
+    ></calc>
+    <calc 
+      label="Shipping" 
+      v-model="shipping"
+      v-bind:currency="currency"
+    ></calc>
+    <calc 
+      label="Handling" 
+      v-model="handling"
+      v-bind:currency="currency"
+    ></calc>
+    <calc 
+      label="Discount" 
+      v-model="discount"
+      v-bind:currency="currency"
+    ></calc>
+    <p>Total: {{ currency }}{{ total }}</p>
+  </div>
+</template>
+
+<script>
+  import Calc from './components/calc'
+  export default {
+    components: {
+      Calc
+    },
+    data: function () {
+      return {
+        currency: '£',
+        price: 10,
+        shipping: 0,
+        handling: 0,
+        discount: 0
+      }
+    },
+    computed: {
+      total: function () {
+        return ((
+          this.price * 100 +
+          this.shipping * 100 +
+          this.handling * 100 -
+          this.discount * 100
+        ) / 100).toFixed(2)
+      }
+    }
+  }
+</script>
---
+++ app/src/components/calc.vue
@@ -0,0 +1,42 @@
+<style module>
+  .test {
+    color: blue;
+  }
+</style>
+
+<template>
+  <div>
+    <label :class="$style.test" v-if="label">{{ label }}</label>
+    {{ currency }}
+    <input
+      ref="input"
+      v-bind:value="value"
+      v-on:input="updateValue($event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'calc',
+    props: {
+      currency: {
+        type: String,
+        default: '$'
+      },
+      value: {
+        type: Number,
+        default: 0
+      },
+      label: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      updateValue: function (value) {
+        this.$emit('input', parseInt(value))
+      }
+    }
+  }
+</script>
